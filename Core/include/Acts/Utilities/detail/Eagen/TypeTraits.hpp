54c6b563d682c3bfe6c44fd6d547ef9ec75a6081
Hadrien G <knights_of_ni@gmx.com>
2020-06-29 12:58:21 +0200

    Clean up type traits

--- Core/include/Acts/Utilities/detail/Eagen/TypeTraits.hpp
+++ Core/include/Acts/Utilities/detail/Eagen/TypeTraits.hpp
@@ -22,7 +22,7 @@ namespace Eagen {
 
 // Handle const types
 template <typename EagenType>
-struct TypeTraits<const EagenType> : public TypeTraits<EagenType> {};
+struct TypeTraits<const EagenType> : TypeTraits<EagenType> {};
 
 // Angle-axis transform type traits
 template <typename _Scalar>
@@ -32,8 +32,12 @@ struct TypeTraits<AngleAxis<_Scalar>> {
 };
 
 // Sub-matrix type traits
-template <typename Derived, int BlockRows, int BlockCols, bool InnerPanel>
-struct TypeTraits<Block<Derived, BlockRows, BlockCols, InnerPanel>> {
+template <typename _Derived, int _BlockRows, int _BlockCols, bool _InnerPanel>
+struct TypeTraits<Block<_Derived, _BlockRows, _BlockCols, _InnerPanel>> {
+    using Derived = _Derived;
+    static constexpr int BlockRows = _BlockRows;
+    static constexpr int BlockCols = _BlockCols;
+    static constexpr bool InnerPanel = _InnerPanel;
 private:
     using DerivedTraits = TypeTraits<Derived>;
     using DerivedInner = typename DerivedTraits::Inner;
@@ -46,44 +50,46 @@ public:
     using Inner = std::conditional_t<std::is_const_v<Derived>,
                                      GenericInner<const DerivedInner>,
                                      GenericInner<DerivedInner>>;
-    using Scalar = typename DerivedTraits::Scalar;
-    static constexpr int Rows = BlockRows;
-    static constexpr int Cols = BlockCols;
+    using Scalar = typename Inner::Scalar;
+    static constexpr int Rows = Inner::RowsAtCompileTime;
+    static constexpr int Cols = Inner::ColsAtCompileTime;
     static constexpr int Options = DerivedTraits::Options;
     static constexpr int MaxRows = Inner::MaxRowsAtCompileTime;
     static constexpr int MaxCols = Inner::MaxColsAtCompileTime;
-    static constexpr int InnerStride = DerivedTraits::InnerStride;
-    static constexpr int OuterStride = DerivedTraits::OuterStride;
+    static constexpr int InnerStride = Inner::InnerStrideAtCompileTime;
+    static constexpr int OuterStride = Inner::OuterStrideAtCompileTime;
 };
 
 // Matrix diagonal wrapper type traits
-template <typename MatrixType, int DiagIndex>
-struct TypeTraits<Diagonal<MatrixType, DiagIndex>> {
+template <typename _MatrixType, int _DiagIndex>
+struct TypeTraits<Diagonal<_MatrixType, _DiagIndex>> {
+    using MatrixType = _MatrixType;
+    static constexpr int DiagIndex = _DiagIndex;
 private:
     using MatrixTypeTraits = TypeTraits<MatrixType>;
     using MatrixTypeInner = typename MatrixTypeTraits::Inner;
 public:
-    using Scalar = typename MatrixTypeInner::Scalar;
-    static constexpr int Rows =
-        std::min(MatrixTypeTraits::Rows - std::max(-DiagIndex, 0),
-                 MatrixTypeTraits::Cols - std::max(DiagIndex, 0));
+    using Inner = Eigen::Diagonal<MatrixTypeInner, DiagIndex>;
+    using Scalar = typename Inner::Scalar;
+    static constexpr int Rows = Inner::RowsAtCompileTime;
     static constexpr int Cols = 1;
     static constexpr int Options = MatrixTypeTraits::Options;
-    static constexpr int MaxRows =
-        std::min(MatrixTypeTraits::MaxRows - std::max(-DiagIndex, 0),
-                 MatrixTypeTraits::MaxCols - std::max(DiagIndex, 0));
+    static constexpr int MaxRows = Inner::ColsAtCompileTime;
     static constexpr int MaxCols = 1;
-    using Inner = Eigen::Diagonal<MatrixTypeInner, DiagIndex>;
+    static constexpr int InnerStride = Inner::InnerStrideAtCompileTime;
+    static constexpr int OuterStride = Inner::OuterStrideAtCompileTime;
 };
 
 // Diagonal matrix type traits
 template <typename _Scalar, int _Size, int _MaxSize>
-struct TypeTraits<DiagonalMatrix<_Scalar, _Size, _MaxSize>> {
+struct TypeTraits<DiagonalMatrix<_Scalar, _Size, _MaxSize>>
+    : TypeTraits<Matrix<_Scalar, _Size, _Size, 0, _MaxSize, _MaxSize>>
+{
     using Scalar = _Scalar;
     static constexpr int Size = _Size;
     static constexpr int MaxSize = _MaxSize;
-    using DiagonalVectorType = Matrix<Scalar, Size, 1, 0, MaxSize, 1>;
     using Inner = Eigen::DiagonalMatrix<Scalar, Size, MaxSize>;
+    using DiagonalVectorType = Matrix<Scalar, Size, 1, 0, MaxSize, 1>;
 };
 
 // Diagonal matrix vector wrapper type traits
@@ -92,27 +98,30 @@ struct TypeTraits<DiagonalWrapper<_DiagonalVectorType>> {
     using DiagonalVectorType = _DiagonalVectorType;
 private:
     using DiagonalVectorTraits = TypeTraits<DiagonalVectorType>;
-public:
-    using Scalar = typename DiagonalVectorTraits::Scalar;
-    static constexpr int Size = std::max(DiagonalVectorTraits::Rows,
-                                         DiagonalVectorTraits::Cols);
-    static constexpr int MaxSize = std::max(DiagonalVectorTraits::MaxRows,
-                                            DiagonalVectorTraits::MaxCols);
-private:
-    using DiagonalVectorTypeInner = typename DiagonalVectorType::Inner;
-    template <typename _VectorInner>
-    using GenericInner = Eigen::DiagonalWrapper<_VectorInner>;
+    using DiagonalVectorTypeInner = typename DiagonalVectorTraits::Inner;
+    template <typename _DiagonalVectorTypeInner>
+    using GenericInner = Eigen::DiagonalWrapper<_DiagonalVectorTypeInner>;
 public:
     using Inner =
         std::conditional_t<std::is_const_v<DiagonalVectorType>,
                            GenericInner<const DiagonalVectorTypeInner>,
                            GenericInner<DiagonalVectorTypeInner>>;
+    using Scalar = typename Inner::Scalar;
+    static constexpr int Size = std::max(DiagonalVectorTraits::Rows,
+                                         DiagonalVectorTraits::Cols);
+    static constexpr int Rows = Size;
+    static constexpr int Cols = Size;
+    static constexpr int MaxSize = std::max(DiagonalVectorTraits::MaxRows,
+                                            DiagonalVectorTraits::MaxCols);
+    static constexpr int MaxRows = MaxSize;
+    static constexpr int MaxCols = MaxSize;
 };
 
 // Jacobi SVD decomposition type traits
 template <typename _MatrixType, int _QRPreconditioner>
-struct TypeTraits<JacobiSVD<_MatrixType, _QRPreconditioner>> {
-public:
+struct TypeTraits<JacobiSVD<_MatrixType, _QRPreconditioner>>
+    : TypeTraits<_MatrixType>
+{
     using MatrixType = _MatrixType;
     static constexpr int QRPreconditioner = _QRPreconditioner;
 private:
@@ -123,7 +132,7 @@ public:
 
 // Standard Cholesky decomposition type traits
 template <typename _MatrixType, int _UpLo>
-struct TypeTraits<LLT<_MatrixType, _UpLo>> {
+struct TypeTraits<LLT<_MatrixType, _UpLo>> : TypeTraits<_MatrixType> {
     using MatrixType = _MatrixType;
     static constexpr int UpLo = _UpLo;
 private:
@@ -134,7 +143,7 @@ public:
 
 // Robust Cholesky decomposition type traits
 template <typename _MatrixType, int _UpLo>
-struct TypeTraits<LDLT<_MatrixType, _UpLo>> {
+struct TypeTraits<LDLT<_MatrixType, _UpLo>> : TypeTraits<_MatrixType> {
     using MatrixType = _MatrixType;
     static constexpr int UpLo = _UpLo;
 private:
@@ -144,8 +153,11 @@ public:
 };
 
 // Mapped array type traits
-template <typename Derived, int MapOptions, typename StrideType>
-struct TypeTraits<Map<Derived, MapOptions, StrideType>> {
+template <typename _Derived, int _MapOptions, typename _StrideType>
+struct TypeTraits<Map<_Derived, _MapOptions, _StrideType>> {
+    using Derived = _Derived;
+    static constexpr int MapOptions = _MapOptions;
+    using StrideType = _StrideType;
 private:
     using DerivedTraits = TypeTraits<Derived>;
     using DerivedInner = typename DerivedTraits::Inner;
@@ -155,14 +167,14 @@ public:
     using Inner = std::conditional_t<std::is_const_v<Derived>,
                                      GenericInner<const DerivedInner>,
                                      GenericInner<DerivedInner>>;
-    using Scalar = typename DerivedTraits::Scalar;
+    using Scalar = typename Inner::Scalar;
     static constexpr int Rows = Inner::RowsAtCompileTime;
     static constexpr int Cols = Inner::ColsAtCompileTime;
     static constexpr int Options = DerivedTraits::Options;
     static constexpr int MaxRows = Inner::MaxRowsAtCompileTime;
     static constexpr int MaxCols = Inner::MaxColsAtCompileTime;
-    static constexpr int InnerStride = StrideType::InnerStrideAtCompileTime;
-    static constexpr int OuterStride = StrideType::OuterStrideAtCompileTime;
+    static constexpr int InnerStride = Inner::InnerStrideAtCompileTime;
+    static constexpr int OuterStride = Inner::OuterStrideAtCompileTime;
 };
 
 // Matrix type traits
@@ -179,9 +191,9 @@ struct TypeTraits<Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>> {
     static constexpr int Options = _Options;
     static constexpr int MaxRows = _MaxRows;
     static constexpr int MaxCols = _MaxCols;
-    static constexpr int InnerStride = 1;
-    static constexpr int OuterStride = (Options & RowMajor) ? Cols : Rows;
     using Inner = Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols>;
+    static constexpr int InnerStride = Inner::InnerStrideAtCompileTime;
+    static constexpr int OuterStride = Inner::OuterStrideAtCompileTime;
 };
 
 // Quaternion type traits
@@ -192,9 +204,18 @@ struct TypeTraits<Quaternion<_Scalar, _Options>> {
     using Inner = Eigen::Quaternion<Scalar, Options>;
 };
 
+// 2D rotation type traits
+template <typename _Scalar>
+struct TypeTraits<Rotation2D<_Scalar>> {
+    using Scalar = _Scalar;
+    using Inner = Eigen::Rotation2D<Scalar>;
+};
+
 // Sub-vector type traits
-template <typename Derived, int Size>
-struct TypeTraits<VectorBlock<Derived, Size>> {
+template <typename _Derived, int _Size>
+struct TypeTraits<VectorBlock<_Derived, _Size>> {
+    using Derived = _Derived;
+    static constexpr int Size = _Size;
 private:
     using DerivedTraits = TypeTraits<Derived>;
     using DerivedInner = typename DerivedTraits::Inner;
@@ -204,21 +225,14 @@ public:
     using Inner = std::conditional_t<std::is_const_v<Derived>,
                                      GenericInner<const DerivedInner>,
                                      GenericInner<DerivedInner>>;
-    using Scalar = typename DerivedTraits::Scalar;
+    using Scalar = typename Inner::Scalar;
     static constexpr int Rows = Inner::RowsAtCompileTime;
     static constexpr int Cols = Inner::ColsAtCompileTime;
     static constexpr int Options = DerivedTraits::Options;
     static constexpr int MaxRows = Inner::MaxRowsAtCompileTime;
     static constexpr int MaxCols = Inner::MaxColsAtCompileTime;
-    static constexpr int InnerStride = DerivedTraits::InnerStride;
-    static constexpr int OuterStride = DerivedTraits::OuterStride;
-};
-
-// 2D rotation type traits
-template <typename _Scalar>
-struct TypeTraits<Rotation2D<_Scalar>> {
-    using Scalar = _Scalar;
-    using Inner = Eigen::Rotation2D<Scalar>;
+    static constexpr int InnerStride = Inner::InnerStrideAtCompileTime;
+    static constexpr int OuterStride = Inner::OuterStrideAtCompileTime;
 };
 
 }  // namespace Eagen
