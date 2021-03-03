35719830a52bde46ab520104848fa3826c45e807
Hadrien G <knights_of_ni@gmx.com>
2021-03-03 11:22:51 +0100

    Hopefully complete implementation of ROOT file readout

--- Examples/Run/Reproducibility/ReadRootFile.cpp
+++ Examples/Run/Reproducibility/ReadRootFile.cpp
@@ -11,17 +11,144 @@
 #include <utility>
 #include <vector>
 
-#include <RTypesCore.h>
-#include <TBranch.h>
 #include <TFile.h>
-#include <TKey.h>
 #include <TList.h>
 #include <TObject.h>
-#include <TTree.h>
-#include <TTreeReader.h>
 
 #include "ErrorHandling.hpp"
 #include "ReadRootFile.hpp"
+
+
+BranchProperties(TBranch& branch)
+  : className(branch.GetClassName())
+{
+  TClass* unused;
+  branch.GetExpectedType(unused, this->dataType);
+  ASSERT_EQ(branch.GetNleaves(), 1, "Multi-leaf branches are not supported");
+}
+
+
+std::unique_ptr<BranchDataReader> setup(TTreeReader& treeReader,
+                                        size_t numEntries,
+                                        TBranch& branch) {
+  BranchProperties branchProperties{ branch };
+  const std::string& className = branchProperties.className;
+  const char* const branchName = branch.GetName();
+
+  // Unlike ROOT, we can't do runtime code generation, so we must define in
+  // advance what kind of ROOT branch types we're going to support and
+  // pre-instantiate TypedBranchDataReaders for all those types.
+  #define INSTANTIATE(cppType) \
+    return std::make_unique<TypedBranchDataReader<cppType>>( \
+        treeReader, numEntries, std::move(branchProperties), branchName)
+
+  #define TYPE_CASE(rootType, cppType) \
+    case rootType: INSTANTIATE(cppType)
+
+  switch (branchProperties.dataType) {
+    TYPE_CASE(kChar_t, char);
+    TYPE_CASE(kUChar_t, unsigned char);
+    TYPE_CASE(kShort_t, short);
+    TYPE_CASE(kUShort_t, unsigned short);
+    TYPE_CASE(kInt_t, int);
+    TYPE_CASE(kUInt_t, unsigned int);
+    TYPE_CASE(kLong_t, long);
+    TYPE_CASE(kULong_t, unsigned long);
+    TYPE_CASE(kULong64_t, unsigned long long);
+    TYPE_CASE(kFloat_t, float);
+    TYPE_CASE(kDouble_t, double);
+    TYPE_CASE(kBool_t, bool);
+    case kOther_t:
+      if (className.substr(0, 6) == "vector") {
+        std::string elementType = className.substr(7, className.size() - 8);
+
+        #define HANDLE_VECTOR(thisElementType) \
+          if (elementType == #thisElementType) { \
+            INSTANTIATE(std::vector<thisElementType>); \
+          } else
+
+        #define HANDLE_INTEGER_VECTOR(intElementType) \
+          HANDLE_VECTOR(signed intElementType) \
+          HANDLE_VECTOR(unsigned intElementType)
+
+        HANDLE_INTEGER_VECTOR(char)
+        HANDLE_INTEGER_VECTOR(short)
+        HANDLE_INTEGER_VECTOR(int)
+        HANDLE_INTEGER_VECTOR(long)
+        HANDLE_INTEGER_VECTOR(long long)
+        HANDLE_VECTOR(float)
+        HANDLE_VECTOR(double)
+        HANDLE_VECTOR(bool)
+        THROW("Unsupported std::vector element type " << elementType);
+
+        #undef HANDLE_INTEGER_VECTOR
+        #undef HANDLE_VECTOR
+      } else {
+        THROW("Unsupported branch class " << className);
+      }
+    default:
+      THROW("Unsupported ROOT branch type " << branchProperties.dataType);
+  }
+
+  #undef TYPE_CASE
+  #undef INSTANTIATE
+}
+
+
+TreeData::TreeData(TTree& tree)
+  : numEntries(tree.GetEntries())
+{
+  // Read tree branches
+  std::vector<TBranch*> branches;
+  {
+    const Int_t branchCount = tree.GetNbranches();
+    branches.reserve(branchCount);
+    TIter branchIter{tree.GetListOfBranches()};
+    for (int i = 0; i < t1BranchCount; ++i) {
+      branches.emplace_back(dynamic_cast<TBranch*>(branchIter()));
+    }
+  }
+
+  // Prepare for tree readout
+  TTreeReader treeReader(&tree);
+  std::unordered_map<std::string, std::unique_ptr<BranchDataReader>> branchReaders;
+  branchReaders.reserve(branches.size());
+  for (const auto branchPtr : std::move(branches)) {
+    branchReaders.push_back(
+      branchPtr->GetName(),
+      BranchDataReader::setup(treeReader, this->numEntries, *branchPtr)
+    );
+  }
+
+  // Read event data
+  for (size_t i = 0; i < this->numEntries; ++i) {
+    treeReader.Next();
+    for (auto& [_, branchReader] : branchReaders) {
+      branchReader.collectValue();
+    }
+  }
+
+  // Collect the event data
+  this->branchData.reserve(branchReaders.size());
+  for (auto&& [branchName, branchReader] : std::move(branchReaders)) {
+    this->branchData.insert(branchName, branchReader.finish());
+  }
+}
+
+
+LatestKeyCycleData::LatestKeyCycleData(TKey& key)
+  : version(key.GetVersion())
+  , className(key.GetClassName())
+  , title(key.GetTitle())
+{
+  // Assert that the data is a TTree (only supported type at the moment)
+  TObject& obj = key.ReadObj();
+  ASSERT_EQ(obj.ClassName(), "TTree", "Unsupported TKey type");
+  TTree& tree = dynamic_cast<TTree&>(obj);
+
+  // Load data from the tree
+  this->data = TreeData(tree);
+}
 
 
 FileData::FileData(const std::string& fileName) {
@@ -55,6 +182,7 @@ FileData::FileData(const std::string& fileName) {
     auto latestCycleIter = latestKeys.find(keyName);
     if (latestCycleIter != latestKeys.end()) {
       KeyCycle& latestCycleData = latestCycleIter->second;
+      ASSERT_NE(keyCycle, latestCycleData.first, "(cycle, name) pairs should be unique");
       if (keyCycle > latestCycleData.first) {
         latestCycleData = KeyCycle{ keyCycle, key };
       }
@@ -63,29 +191,8 @@ FileData::FileData(const std::string& fileName) {
     }
   }
 
-  // Process the latest keys
+  // Load data from the latest cycle of each key
   for (const auto [keyName, keyCycle] : std::move(latestKeys)) {
-    // Check that latest keys do map to TTrees and access them
-    TKey& key = *keyCycle->second;
-    TObject& obj = key.ReadObj();
-    ASSERT_EQ(obj.ClassName(), "TTree", "Unsupported TKey type");
-    TTree& tree = dynamic_cast<TTree&>(obj);
-
-    // Read tree branches
-    std::vector<TBranch*> branches;
-    {
-      const Int_t branchCount = tree.GetNbranches();
-      branches.reserve(branchCount);
-      TIter branchIter{tree.GetListOfBranches()};
-      for (int i = 0; i < t1BranchCount; ++i) {
-        branches.emplace_back(dynamic_cast<TBranch*>(branchIter()));
-      }
-    }
-
-    // Prepare for tree readout
-    TTreeReader treeReader(&tree);
-
-    // TODO: Finish translating compareRootFiles.C functionality
-    // TODO: Recursively fill in this->keys
+    this->keys.emplace(keyName, *keyCycle->second);
   }
 }
--- Examples/Run/Reproducibility/ReadRootFile.hpp
+++ Examples/Run/Reproducibility/ReadRootFile.hpp
@@ -11,33 +11,69 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include <RTypesCore.h>
+#include <TBranch.h>
 #include <TDictionary.h>
+#include <TKey.h>
+#include <TTree.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
 
 
-/// Abstract interface to BranchData + common subset of all BranchData types
-struct BranchDataBase {
+// --- OUTPUT DATA ---
+
+/// Useful type-agnostic properties of a ROOT TBranch
+struct BranchProperties {
   std::string className;
-  EDataType type;
-  static constexpr Int_t nLeaves = 1;  // >1 leaves is not currently supported
+  EDataType dataType;
+  static constexpr Int_t numLeaves = 1;  // >1 leaves is not currently supported
 
-  // TODO: Abstract interface to higher-level functionality
+  /// Extract a ROOT TBranch's properties
+  BranchProperties(TBranch& branch);
+
+  BranchProperties(BranchProperties&&) = default;
+  BranchProperties& operator=(BranchProperties&&) = default;
+};
+
+/// Type-erased interface to TypedBranchData
+struct BranchData : BranchProperties {
+  /// Forward branch properties down the inheritance stack
+  BranchData(BranchProperties&& properties)
+    : BranchProperties{ std::move(properties) }
+  {}
+
+  virtual ~BranchData() = default;
+  // TODO: More abstract interface to higher-level functionality
 };
 
 /// Concrete data that was extracted from a ROOT TTree's Branch
 template <typename T>
-struct BranchData : BranchDataBase {
+struct TypedBranchData : BranchData {
   std::vector<T> data;
 
-  // TODO: Implementation of the BranchDataBase interface
+  /// Prepare to record a number of entries from a certain branch
+  TypedBranchData(BranchProperties&& properties, size_t numEntries)
+    : BranchData{ std::move(properties) }
+  {
+    data.reserve(numEntries);
+  }
+
+  // TODO: Implementation of the abstract BranchData API as it comes in
 };
 
 /// Data from a ROOT TTree
 struct TreeData {
-  size_t numEntries;
-  std::unordered_map<std::string, std::unique_ptr<BranchDataBase>> branches;
+  size_t numEntries = 0;
+  std::unordered_map<std::string, std::unique_ptr<BranchData>> branchData;
+
+  /// Needed because constructors are evil
+  TreeData() = default;
+
+  /// Load data from a ROOT TTree
+  TreeData(TTree& tree);
 };
 
 /// This code currently only supports TTree data
@@ -54,6 +90,9 @@ struct LatestKeyCycleData {
   std::string className;
   std::string title;
   KeyData data;
+
+  /// Load data from a ROOT TKey
+  LatestKeyCycleData(TKey& key);
 }
 
 /// Data from a ROOT file
@@ -61,5 +100,56 @@ struct FileData {
   Int_t version;
   std::unordered_map<std::string, LatestKeyCycleData> keys;
 
+  /// Load data from a ROOT file
   FileData(const std::string& fileName);
 };
+
+
+// --- DATA READOUT MECHANISM ---
+
+/// Type-erased interface to TypedBranchDataReader
+class BranchDataReader {
+public:
+  virtual ~BranchDataReader() = default;
+
+  /// Prepare to load data from a TTree branch
+  static std::unique_ptr<BranchDataReader> setup(TTreeReader& treeReader,
+                                                 size_t numEntries,
+                                                 TBranch& branch);
+
+  /// Record current branch data
+  ///
+  /// This method must be called for each recorded branch, after every call to
+  /// the underlying TTreeReader's Next() method.
+  ///
+  virtual void collectValue() = 0;
+
+  /// Dispose of this data readout harness and get the recorded data
+  virtual std::unique_ptr<BranchData> finish() = 0;
+};
+
+/// Mechanism to read TypedBranchData using a TTreeReader
+template <typename T>
+class TypedBranchDataReader : public BranchDataReader {
+public:
+  TypedBranchDataReader(TTreeReader& reader,
+                        size_t numEntries,
+                        BranchProperties&& branchProperties,
+                        const char* branchName)
+    : m_data{ std::make_unique<TypedBranchData<T>>(std::move(branchProperties),
+                                                   numEntries) }
+    , m_reader{ reader, branchName }
+  {}
+
+  void collectValue() final override {
+    m_data.data.push_back(*m_reader);
+  }
+
+  std::unique_ptr<BranchData> finish() final override {
+    return std::move(m_data);
+  }
+
+private:
+  std::unique_ptr<TypedBranchData<T>> m_data;
+  TTreeReaderValue<T> m_reader;
+}
